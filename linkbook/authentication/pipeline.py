ef4f9f1f38eda4a8ab7ac085f68e67ac071d9d89
nikhilkumarsingh <nikhilksingh97@gmail.com>
2017-06-24 23:39:23 +0530

    bigger fb profile pic

--- linkbook/authentication/pipeline.py
+++ linkbook/authentication/pipeline.py
@@ -11,7 +11,7 @@ def save_profile(backend, user, response, *args, **kwargs):
 		elif backend.name == "twitter":
 			pic = "https://twitter.com/{}/profile_image?size=normal".format(response.get('screen_name'))
 		elif backend.name == "facebook":
-			pic = "https://graph.facebook.com/{}/picture?type=square".format(response.get('id'))
+			pic = "https://graph.facebook.com/{}/picture?type=large&height=720&width=720".format(response.get('id'))
 		elif backend.name == "google-oauth2":
 			try:
 				r = requests.get("http://picasaweb.google.com/data/entry/api/user/{}?alt=json".format(response.get('id')))
