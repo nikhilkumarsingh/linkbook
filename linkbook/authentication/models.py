83bd8b642794a6233bf4cd96363cfd6edd8b0c5a
nikhilkumarsingh <nikhilksingh97@gmail.com>
2017-06-25 23:43:44 +0530

    minor fix

--- linkbook/authentication/models.py
+++ linkbook/authentication/models.py
@@ -6,7 +6,7 @@ from django.db.models.signals import post_save
 class Profile(models.Model):
 	user = models.OneToOneField(User, related_name='profile', primary_key=True)
 	pic = models.URLField(default = "http://i.imgur.com/xz4vNF9.png")
-	backend = models.CharField(max_length = 10, default = "linkbook")
+	backend = models.CharField(max_length = 20, default = "linkbook")
 	followers = models.ManyToManyField('self', related_name='following', symmetrical=False)
 	def __str__(self):
 		return self.user.username
