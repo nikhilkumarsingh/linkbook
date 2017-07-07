552b7c9bbbd680abf9ae5e2e215df1f6e1a98ca6
nikhilkumarsingh <nikhilksingh97@gmail.com>
2017-07-08 01:46:25 +0530

    removed login required form view tag

--- linkbook/links/views.py
+++ linkbook/links/views.py
@@ -280,7 +280,6 @@ def ajax_delete_comment(request, id):
         return JsonResponse({'done': True})
 
 
-@login_required
 def view_tag(request, id, tag_name):
     tagged = Link.objects.filter(tags__name = tag_name)
     return render(request, "links/tags.html/", {'tag': tagged, 'tagname' : tag_name})
