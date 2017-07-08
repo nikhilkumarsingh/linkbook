50a01b59291e78ffeaf71059cd0fe00439adb4c1
himanshugupta1997 <himanshugupta1997@gmail.com>
2017-07-08 21:11:15 +0530

    Removed console.log statements

--- linkbook/static/library/js/index.js
+++ linkbook/static/library/js/index.js
@@ -36,10 +36,10 @@
                 if($(this).html().length > max_length)
                 {
 
-                    console.log("Read_more");
+                    // console.log("Read_more");
                     var short_content = $(this).html().substr(0, max_length);
                     $(this).html(short_content);
-                    console.log($(this).next().children()[0]);
+                    // console.log($(this).next().children()[0]);
                     $(this).next().children().css({'display' : 'inline-block'});
                 }
 
@@ -50,13 +50,13 @@
             /* Events for edit and delete Comments */
             $('.editCommentButton').click(function () {
 
-                console.log("Edit comment called");
+                // console.log("Edit comment called");
                 var id = this.id;
                 id = id.split('');
-                console.log(id);
+                // console.log(id);
                  id.splice(0, 17);
                 id = id.join('');
-                console.log(id);
+                // console.log(id);
                 var textAreaEditComment = $('#modalEditCommentTextarea'  +id);
                 var text = textAreaEditComment.val();
                 if(!$.trim(text)){
@@ -73,7 +73,7 @@
                     type: 'post',
                     data: {'text':text},
                     success :function(data, status){
-                        console.log(data);
+                        // console.log(data);
                         $('#modalEditComment' + id).modal('close');
                         LoadComments(id);
                     }
@@ -84,7 +84,7 @@
             var show_url_button = $('.show_url_button');
             show_url_button.click(function () {
                 var str = 'link_url' + (this.id);
-                console.log(str);
+                // console.log(str);
                 selectText(str);
             });
 
@@ -93,17 +93,17 @@
 
                 var id = this.id;
                 id = id.split('');
-                console.log(id);
+                // console.log(id);
                  id.splice(0, 19);
                 id = id.join('');
-                console.log(id);
+                // console.log(id);
 
                  commentIndex = commentsOperationsVariable;
                 $.ajax({
                     url : '/comment/'+commentIndex+'/delete/',
                     type: 'post',
                     success :function(data, status){
-                        console.log(data);
+                        // console.log(data);
                         $('#modalEditComment' + id).modal('close');
                         LoadComments(id);
                     }
@@ -112,12 +112,12 @@
             $('.modal').modal();
             var copy = $('.copy');
             copy.click(function (){
-                console.log("Copied Called");
+                // console.log("Copied Called");
                 var id = this.id;
-                console.log("id is " + id);
+                // console.log("id is " + id);
                 var linkUrl = $('#link_url' + id).attr('href');
-                console.log("LinkUrl is");
-                console.log(linkUrl);
+                // console.log("LinkUrl is");
+                // console.log(linkUrl);
                 copyToClipboard(linkUrl);
             });
 
