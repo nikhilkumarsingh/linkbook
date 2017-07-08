9e8f37db2153dd93b574aab5b0a7655cf3d15df8
nikhilkumarsingh <nikhilksingh97@gmail.com>
2017-07-08 21:15:09 +0530

    minor fix

--- linkbook/authentication/models.py
+++ linkbook/authentication/models.py
@@ -9,7 +9,7 @@ class Profile(models.Model):
 	user = models.OneToOneField(User, related_name='profile', primary_key=True)
 	pic = models.URLField(default = "http://i.imgur.com/xz4vNF9.png")
 	backend = models.CharField(max_length = 20, default = "linkbook")
-	followers = models.ManyToManyField('self', related_name='following', symmetrical=False)
-	upvoted_links = models.ManyToManyField(Link)
+	followers = models.ManyToManyField('self', related_name='following', symmetrical=False, blank = True)
+	upvoted_links = models.ManyToManyField(Link, blank = True)
 	def __str__(self):
 		return self.user.username
