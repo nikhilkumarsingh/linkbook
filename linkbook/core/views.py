f52967629c7eb1c56a3704aa1593969278121eb3
nikhilkumarsingh <nikhilksingh97@gmail.com>
2017-07-08 16:40:19 +0530

    sorting popular links

--- linkbook/core/views.py
+++ linkbook/core/views.py
@@ -147,8 +147,9 @@ def username_slugs(request, username):
         return render(request, 'links/view_upvoted_links.html', {'links':upvoted_links})        
 
     else:
-        links = Link.objects.filter(user = user)[:5]
-        books = Book.objects.filter(user = user)[:5]
+        links = Link.objects.filter(user = user).order_by('-num_vote_up', 
+            'num_vote_down',)[:4]
+        books = Book.objects.filter(user = user)[:4]
         link_count = Link.objects.filter(user = user).count()
         book_count = Book.objects.filter(user = user).count()
         follower_count = user.profile.followers.count()
